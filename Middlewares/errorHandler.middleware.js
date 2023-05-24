b9d51611355f24ec90b008b293553b0a40ec8902
rahul shukla <36674663+rahul778620@users.noreply.github.com>
2023-05-24 20:51:17 +0100

    Added Error Object for logging

--- Middlewares/errorHandler.middleware.js
+++ Middlewares/errorHandler.middleware.js
@@ -3,25 +3,24 @@ const { logger } = require('../Logger/logger');
 
 module.exports.errorHandler = async (err, req, res, next) => {
     console.log("In Middlware");
-    const functionName = err.functionName;
-    const controllerName = err.controllerName;
+    const parameters = { "body ": { ...req.body }, "params": { ...req.params }, "query": { ...req.query } };  
+    var errorObject = {
+        "errorInUrl" : `${req.method} ${req.headers.host} ${req.url}`,
+        "inputParams" : parameters,
+        "controllerName" : err.controllerName,
+        "functionName" : err.functionName,
+        "errorMessage" : err.message,        
+        "stackTrace" : err.stack,
+    };
 
-    logger.error(`Controller Function Name ${controllerName}`);
-
-    const parameters = { "body ": { ...req.body }, "params": { ...req.params }, "query": { ...req.query } };
     if (err instanceof AxiosError) {
-        console.log("In Axios Error");
-        logger.error(`Error in URL ${req.url}`)
-        logger.error(`Error in ${functionName}: ${err.message}`);
-        logger.error(`Function parameters:`, parameters);
-        res.status(err.statusCode).json({ error: err.message, data: err.data });
+        console.log("In Axios Error");                        
+        res.status(err.statusCode).json({ error: err.message, data: err.data.trim() });
     } else {        
-        console.log("In Other Errors");
-        logger.error(`Error in URL ${req.url}`)
-        logger.error(`Error in function ${functionName} : ${err.message}`);        
-        logger.error(`Function parameters:`, parameters);        
-        logger.error(`Stack trace:`, err.stack);
+        console.log("In Other Errors");                                          
         res.status(500).json({ error: 'Internal Server Error' });
     }
+
+    logger.error(JSON.stringify(errorObject));
 
 }
